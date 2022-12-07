22cec350a039f13da4ddf7e55a4cd1c45e971e9c
uczen <mateusz.mleczko@uczen.ckziu.net>
2022-12-07 22:24:34 +0100

    Initial commit

---
+++ adStudent.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form action="adStudent.php" method="post">
+        <div class="form__inputs">
+            <input type="text" name="fname" placeholder="Enter ur name" required>
+            <input type="text" name="sname" placeholder="Enter ur surname" required>
+            <input type="submit">
+
+            <?php
+
+            session_start();
+            
+            $servername = "localhost";
+            $username = "root";
+            $password= "";
+            
+            $conn = mysqli_connect($servername, $username, $password, 'school2');
+
+            if (!$conn) {
+                error_log("Failed to connect to mysql: " . mysqli_error($conn));
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            else{
+                mysqli_select_db($conn, 'school2');
+                
+
+                if(isset($_POST["fname"]) && isset($_POST["sname"])){
+                    
+                    $fname = $_POST['fname'];
+                    $sname = $_POST['sname'];
+
+                    $sql = "INSERT INTO student(fname, surname)  VALUES ('$fname', '$sname')";
+                        if($conn -> query($sql)) {
+                        echo "New record created successfully"; 
+                        }
+                }        
+            }
+        ?>
+        </div>
+
+    </form>
+</body>
+</html>
